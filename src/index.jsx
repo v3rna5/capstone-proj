0c610a8012b8d100ecad5184eeb00636f874f7c7
verna <vernajs@gmail.com>
2018-07-13 15:13:19 -0700

    Update name folders

---
+++ src/index.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+import { AppContainer } from 'react-hot-loader';
+import { HashRouter } from 'react-router-dom';
+import { createStore } from 'redux';
+import propertyListReducer from './reducers/property-list-reducer';
+import { Provider } from 'react-redux';
+import rootReducer from './reducers/index';
+
+
+const store = createStore(rootReducer);
+
+let unsubscribe = store.subscribe(() =>
+  console.log(store.getState())
+);
+
+
+const render = (Component) => {
+  ReactDOM.render(
+      <HashRouter>
+        <Provider store={store}>
+        <Component/>
+        </Provider>
+      </HashRouter>,
+
+    document.getElementById('react-app-root')
+  );
+};
+
+render(App);
+
+/*eslint-disable */
+if (module.hot) {
+  module.hot.accept('./components/App', () => {
+    render(App);
+  });
+}
+/*eslint-enable */
