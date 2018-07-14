00ec07bdba17e3931729c224a87fdcd3c36b962d
verna <vernajs@gmail.com>
2018-07-13 17:17:09 -0700

    Fix errors

--- src/reducers/property-list-reducer.js
+++ src/reducers/property-list-reducer.js
@@ -1,10 +1,31 @@
 import c from './../constants';
 
 export default (state = {}, action) => {
+  let newState;
+  const { names, location, issue, timeOpen, id, formattedWaitTime } = action;
+
   switch (action.type) {
-  case c.SELECT_PROPERTY:
-    return action.propertyId;
-  default:
+    case c.ADD_PROPERTY:
+    newState = Object.assign({}, state, {
+      [id]: {
+        names: names,
+        location: location,
+        issue: issue,
+        timeOpen: timeOpen,
+        id: id,
+        formattedWaitTime: formattedWaitTime
+      }
+    });
+    return newState;
+
+    case c.UPDATE_TIME:
+    const newProperty = Object.assign({}, state[id], {formattedWaitTime});
+    newState = Object.assign({}, state, {
+      [id]: newProperty
+    });
+    return newState;
+
+    default:
     return state;
   }
 };
--- src/reducers/selected-property-reducer.js
+++ src/reducers/selected-property-reducer.js
@@ -1,31 +1,10 @@
 import c from './../constants';
 
 export default (state = {}, action) => {
-  let newState;
-  const { names, location, issue, timeOpen, id, formattedWaitTime } = action;
-
   switch (action.type) {
-    case c.ADD_PROPERTY:
-    newState = Object.assign({}, state, {
-      [id]: {
-        names: names,
-        location: location,
-        issue: issue,
-        timeOpen: timeOpen,
-        id: id,
-        formattedWaitTime: formattedWaitTime
-      }
-    });
-    return newState;
-
-    case c.UPDATE_TIME:
-    const newProperty = Object.assign({}, state[id], {formattedWaitTime});
-    newState = Object.assign({}, state, {
-      [id]: newProperty
-    });
-    return newState;
-
-    default:
+  case c.SELECT_PROPERTY:
+    return action.propertyId;
+  default:
     return state;
   }
 };
